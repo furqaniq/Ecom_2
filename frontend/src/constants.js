3331c81d1a778de4bbfff64ebe8cb313b129caa7
furqaniq <furqan93@gmail.com>
2024-12-19 00:57:25 +0500

    base url edit

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '' ;
-export const BASE_URL = 'https://ecom-2-eight.vercel.app/api';
+export const BASE_URL = 'https://ecom-2-eight.vercel.app';
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
